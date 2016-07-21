d29386f898cfbc8c4ec838c3c78f1b11dcb25e6e
phil <philiperickson@gmail.com>
2016-07-20 19:56:57 -0600

    ignore build files

--- .build/versionData/Nifty.swift
+++
@@ -1,3 +0,0 @@
-public let url: String = "https://github.com/philipce/nifty.git"
-public let version: (major: Int, minor: Int, patch: Int, prereleaseIdentifiers: [String], buildMetadata: String?) = (1, 0, 0, [], nil)
-public let versionString: String = "1.0.0"
